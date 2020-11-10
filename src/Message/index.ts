b3deffe6bbc1949ed3421307e8fc5a9ad092597e
Hariom Sharma <hariom@appointy.com>
2020-11-10 15:17:44 +0530

    added type to message

--- src/Message/index.ts
+++ src/Message/index.ts
@@ -7,6 +7,7 @@ interface MessageData {
   id: number | string;
   message: string;
   senderName?: string;
+  type?: "message" | "announcement";
 }
 
 export default class Message {
@@ -16,9 +17,11 @@ export default class Message {
   id: number | string;
   message: string;
   senderName?: string;
+  type?: "message" | "announcement";
   constructor(messageData: MessageData) {
     this.id = messageData.id; // id of the sender (0 is reserved for "blue bubble")
     this.message = messageData.message;
     this.senderName = messageData.senderName || undefined;
+    this.type = messageData.type || "message";
   }
 }
